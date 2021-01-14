61eb1140fff55de63e73fff76146a73f475d8e02
Tilo Spannagel <development@tilosp.de>
2021-01-14 14:46:23 +0100

    Remove Result from sync
    Signed-off-by: Tilo Spannagel <development@tilosp.de>

--- src/matrix.rs
+++ src/matrix.rs
@@ -93,7 +93,7 @@ impl MatrixClient {
             client.restore_login(session.into()).await?;
 
             let client = Self::new(client, dirs);
-            client.sync().await?;
+            client.sync().await;
             Some(client)
         } else {
             None
@@ -116,7 +116,7 @@ impl MatrixClient {
         .save(&dirs.session_file)?;
 
         let client = Self::new(client, dirs);
-        client.sync().await?;
+        client.sync().await;
         Ok(client)
     }
 
@@ -133,9 +133,8 @@ impl MatrixClient {
         Ok(())
     }
 
-    pub(crate) async fn sync(&self) -> Result {
-        self.client.sync(SyncSettings::new()).await;
-        Ok(())
+    pub(crate) async fn sync(&self) {
+        self.client.sync(SyncSettings::new()).await
     }
 
     pub(crate) async fn join_room(
